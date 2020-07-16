57ad66410ba5831dffb98b2ade7dae27ad59171c
Spencer <Spencerp11338@gmail.com>
2020-07-16 13:33:08 -0600

    shared repo test

---
+++ GitTest.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.shared;
+
+public class GitTest {
+
+        public static void foo(){
+            System.out.println("this method is shared!");
+        }
+    }
+
+
